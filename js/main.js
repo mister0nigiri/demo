4443bfd0f5277ce792964f631986babdc1e59418
miyashun <gongyongjunyou@gmail.com>
2024-05-15 23:04:13 +0900

    本日の作業終了

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-const swiper = new Swiper('.swiper', {
-
-  direction: 'vertical',
-  loop: true,
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+const swiper = new Swiper(".swiper", {
+  loop: true, 
+  speed: 1500, 
+  slidesPerView: 1.5, 
+  spaceBetween: 30, 
+  centeredSlides: true, 
+  // ページネーション
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
-
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
